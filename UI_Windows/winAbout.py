6297818505713c0be3ca75ecc87bfb9165a982dc
Jacek Kruszniewski <jacek_k_pi@outlook.com>
2025-09-08 13:15:10 +0200

    Ukończenie okna winAbout - z obsługąklknięcie linku do projektu

--- UI_Windows/winAbout.py
+++ UI_Windows/winAbout.py
@@ -1,9 +1,21 @@
 import wx
 import wx.html
+import webbrowser
 
 from Modules.misc import calculate_path
 from Settings.global_var import varGlobals
-from UI_Windows.winDialogs import wxdlg_const
+from UI_Windows.winDialogs import winMessageBox, wxdlg_const
+
+class custonHtmlWindow( wx.html.HtmlWindow):
+    def __init__( parent: wx.Window | None, id: int = wx.ID_ANY, pos: wx.Point = wx.DefaultPosition, size: wx.Size = wx.DefaultSize, style: int = wx.html.HW_DEFAULT_STYLE, name: str = "htmlWindow" ):
+        wx.html.HtmlWindow.__init__( parent, id, pos, size, style, name)
+
+    def OnHTMLLinkClicked(self, link: wx.html.HtmlLinkInfo):
+        winWAsk = winMessageBox("Kliknąłęś na odnośnik do projektyna GitHub.\nCzy otworzyć projekt w preglądarce?","Otwarcie strony GitHub", wxdlg_const.ICON_ASK | wxdlg_const.ID_YES_NO)
+        idAsk = winWAsk.ShowModal()
+        if idAsk == wxdlg_const.ID_YES:
+            webbrowser.open_new_tab( link.GetHref() )
+        winWAsk.Destroy()
 
 
 class winAbout(wx.Dialog):
@@ -23,7 +35,7 @@ class winAbout(wx.Dialog):
         <h2>Autor:</h2>
         <p><b>Jacekk Kruszniewski (JUPIT Jacek Kruszniewsk Usługi Programistyczne i Informatyczne)</b><br>
         {varGlobals.app_year_from} – {varGlobals.app_year_to}</p>
-        <p>Projekt jest dostępny na serwerze GitHub <a href="https://github.com/JUPIT-Jacek-K/InvoiceViewerKSEF" target="_blank"></a></p>
+        <p>Projekt jest dostępny na serwerze GitHub <a href="https://github.com/JUPIT-Jacek-K/InvoiceViewerKSEF" target="_blank">JUPIT-Jacek-K/InvoiceViewerKSEF</a></p>
         <p><br>
         </p>
     </body>
@@ -66,19 +78,24 @@ class winAbout(wx.Dialog):
 
         messageSizer.Add(self.aboutImage, 0, 0, 5)
 
-        self.m_htmlWinAbout = wx.html.HtmlWindow(
+        self.m_htmlWinAbout = custonHtmlWindow( # wx.html.HtmlWindow
             self.infoPanel,
             wx.ID_ANY,
             wx.DefaultPosition,
             wx.Size(400, 400),
             wx.html.HW_NO_SELECTION | wx.html.HW_SCROLLBAR_NEVER,
         )
-        messageSizer.Add(self.m_htmlWinAbout, 0, wx.ALL | wx.EXPAND, 5)
-        self.m_htmlWinAbout.SetStandardFonts(
-            size=10, normal_face="helvetica", fixed_face="Sans Serif"
+        self.infoPanel.SetBackgroundColour(
+            wx.SystemSettings.GetColour(wx.SYS_COLOUR_3DLIGHT)
         )
-
+        messageSizer.Add(self.m_htmlWinAbout, 0, wx.ALL | wx.EXPAND, 5)
         self.m_htmlWinAbout.SetPage(self.htnlInfo)
+        self.m_htmlWinAbout.SetStandardFonts(
+            size=9, normal_face="helvetica", fixed_face="Sans Serif"
+        )
+        self.m_htmlWinAbout.SetHTMLBackgroundColour(
+            wx.SystemSettings.GetColour(wx.SYS_COLOUR_3DLIGHT)
+        )
 
         self.infoPanel.SetSizer(messageSizer)
 
